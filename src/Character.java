45f9e27fd347803f72a7d7cfcc652c282d0489dd
Fahad <fahad.alhelal@emory.edu>
2016-04-17 17:15:35 -0400

    key press update

--- src/Character.java
+++ src/Character.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -21,6 +22,8 @@ public class Character extends GCompound {
 	private int dx;
 	private int dy;
 	public GOval tempOval;
+	private final int MAPWIDTH = 1400;
+	private final int MAPHEIGHT = 1600;
 	
 	
 	public Character(boolean isPlayer, int health, int x, int y, int w, int h) throws IOException{
@@ -33,16 +36,18 @@ public class Character extends GCompound {
 		height = h;
 		inventoryCount = 0;
 		
-		sprite = ImageIO.read(new File("./resources/p1_front.png"));
-		model = new GImage("p1_front.png");
+		//sprite = ImageIO.read(new File(".//resources//p1_front.png"));
+		//model = new GImage(ImageIO.read(new File(".//resources//p1_front.png")));
 		tempOval = new GOval(x,y,w,h);
+		tempOval.setFilled(true);
+		tempOval.setFillColor(Color.BLACK);
 		this.add(tempOval);
 	}
 	
 	public int getHealth(){
 		return health;
 	}
-	
+	/*
 	public int getX(){
 		return posX;
 	}
@@ -57,7 +62,7 @@ public class Character extends GCompound {
 	
 	public int getHeight(){
 		return height;
-	}
+	}*/
 	
 	public int getInventoryCount(){
 		return inventoryCount;
@@ -72,19 +77,23 @@ public class Character extends GCompound {
 	}
 	
 	public boolean inBounds(int x, int y){
-		return (y >= 0) && (y < 600) && (x >= 0) && (x < 800);
+		return (y >= 0) && (y < MAPHEIGHT) && (x >= 0) && (x < MAPWIDTH);
 	}
 
+	
 	public void move(int x){
 		switch(x){
-		case 1: if(inBounds(posX, posY-1)){posY--;}break;//Move Up
-		case 2: if(inBounds(posX, posY+1)){posY++;}break;//Move Down
-		case 3: if(inBounds(posX-1, posY)){posX--;}break;//Move Left
-		case 4: if(inBounds(posX+1, posY)){posX++;}break;//Move Right
+		case 1: if(inBounds(posX, posY-1)){posY--;update();tempOval.setLocation(posX,posY);}break;//Move Up
+		case 2: if(inBounds(posX, posY+1)){posY++;update();tempOval.setLocation(posX,posY);}break;//Move Down
+		case 3: if(inBounds(posX-1, posY)){posX--;update();tempOval.setLocation(posX,posY);}break;//Move Left
+		case 4: if(inBounds(posX+1, posY)){posX++;update();tempOval.setLocation(posX,posY);}break;//Move Right
 		}
+		
 	}
 	
-	
+	public void update(){
+		tempOval.setLocation(posX, posY);
+	}
 	
 	//create door class
 	public void interactWithDoor(Object x){
